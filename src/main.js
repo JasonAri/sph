793cec5270ea0b71cbb9b93727de8f3ebcae8a24
Jason <1058502178@qq.com>
2022-10-27 04:11:37 +0800

    feat(Home):components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
+// 引入三级联动组件---全局组件
+import TypeNav from '@/pages/Home/TypeNav';
+// 注册组件(两个参数：全局组件的名字，哪一个组件。)
+Vue.component(TypeNav.name, TypeNav);
 // 引入路由
 import router from '@/router';
 
